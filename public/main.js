3ac01eec4ca6f01deaad79aed0a7cfcb34526273
Liz Tiller <elizabeth.tiller@gmail.com>
2016-11-14 15:44:39 -0500

    trying the two equallying each other

--- public/main.js
+++ public/main.js
@@ -9,8 +9,8 @@ for (let i = 0; i < 20; i++) {
   //if (i % 3 === 0 && i % 5 === 0) {
     //console.log('fizzbuzz')
   //}
-  if(i % 5 === 0 || i % 3 === 0) {
-    console.log('buzz' || 'fizz')
+  if(i % 5 === 0 === 3 % i) {
+    console.log('fizzbuzz')
   }
   else if (i % 3 === 0) {
     console.log('fizz')
