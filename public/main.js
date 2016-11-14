39ef7565d16f0b0005c2dce5bdb8531ba058e399
Liz Tiller <elizabeth.tiller@gmail.com>
2016-11-14 16:19:52 -0500

    almost there

--- public/main.js
+++ public/main.js
@@ -8,11 +8,27 @@ for (let i = 0; i < 16; i++) {
   //if (i % 3 === 0 && i % 5 === 0) {
     //console.log('fizzbuzz')
   //}
-  if(i % 3 === 0) {
-    console.log('fizz')
+  let out = ''
+
+  if (i % 3 === 0) '' + 'fizz';
+  else if (i % 5 === 0) '' + 'buzz';
+  //if(i % 3 === 0) {
+    // console.log('fizz')
+  //} else if (i % 5 === 0) {
+    // console.log('buzz')
+  else {
+    out = i
+  }
+  console.log(out)
+}
+
+  /*if (i % 3 === 0 && i % 5 === 0){
+    console.log('fizzbuzz')
+  } else if (i % 3 === 0) {
+    console.log ('fizz')
   } else if (i % 5 === 0) {
-    console.log('buzz')
+    console.log ('buzz')
   } else {
     console.log (i)
   }
-}
+} */
