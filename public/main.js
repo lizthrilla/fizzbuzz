e1c0c623e23725b5150a63c97d75565ba773032d
Liz Tiller <elizabeth.tiller@gmail.com>
2016-11-14 16:26:17 -0500

    motherfing fizzbuzz

--- public/main.js
+++ public/main.js
@@ -5,17 +5,9 @@
 document.addEventListener('DOMContentLoaded', main)*/
 
 for (let i = 0; i < 16; i++) {
-  //if (i % 3 === 0 && i % 5 === 0) {
-    //console.log('fizzbuzz')
-  //}
   let out = ''
-
-  if (i % 3 === 0) '' + 'fizz';
-  else if (i % 5 === 0) '' + 'buzz';
-  //if(i % 3 === 0) {
-    // console.log('fizz')
-  //} else if (i % 5 === 0) {
-    // console.log('buzz')
+  if (i % 3 === 0) out += 'fizz';
+  if (i % 5 === 0) out += 'buzz';
   else {
     out = i
   }
